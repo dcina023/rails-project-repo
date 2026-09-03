7532837a7d9f0792e5a705fae0aae7387242c464
+ <+>
2026-09-03 12:41:38 -0400

    Run rubocop -a to clean up any linting offenses

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,44 +1,43 @@
 class PlansController < ApplicationController
   before_action :require_user
   load_and_authorize_resource through: :current_user
-def show
-end
+  def show; end
 
-def new
-  @user = current_user
-  @activities = Activity.all
-end
-
-def create
-  if @plan.save
-    @plan.add_random_activities(3)
-    redirect_to user_plan_path(current_user, @plan)
-  else
+  def new
+    @user = current_user
     @activities = Activity.all
-    render :new, status: :unprocessable_entity
   end
-end
 
-def edit
-  @user = current_user
-end
-
-def update
-  if @plan.update(plan_params)
-    redirect_to user_plan_path(current_user, @plan),
-                notice: "Plan updated successfully."
-  else
-    render :edit, status: :unprocessable_entity
+  def create
+    if @plan.save
+      @plan.add_random_activities(3)
+      redirect_to user_plan_path(current_user, @plan)
+    else
+      @activities = Activity.all
+      render :new, status: :unprocessable_entity
+    end
   end
-end
 
-def destroy
-  @plan.destroy
-  redirect_to user_path(current_user),
-              notice: "Plan was successfully removed."
-end
+  def edit
+    @user = current_user
+  end
 
-  private
+  def update
+    if @plan.update(plan_params)
+      redirect_to user_plan_path(current_user, @plan),
+                  notice: "Plan updated successfully."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @plan.destroy
+    redirect_to user_path(current_user),
+                notice: "Plan was successfully removed."
+  end
+
+private
 
   def plan_params
     params.require(:plan).permit(
